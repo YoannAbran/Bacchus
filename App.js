968830aee277b6e33a5d8aa28c065f64e95e4877
RDemarch <robin.demarch@sfr.fr>
2020-10-27 10:24:14 +0100

    Correction Navigation

--- App.js
+++ App.js
@@ -2,13 +2,15 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import ListBar from '../Bacchus/Components/ListBar';
+import RightBar from './Navigation/Navigation';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ListBar/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {RightBar()}
+    </NavigationContainer>
+
   );
 }
 
